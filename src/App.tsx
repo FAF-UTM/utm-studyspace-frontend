47d165093d210c13be7e937589656d4a9c4ca74d
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-01-24 10:46:09 +0200

    adding jwt

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/HomePage';
+import 'react-toastify/dist/ReactToastify.css';
+import LoginPage from "./pages/login/LoginPage";
+import HomePage from "./pages/home/HomePage";
+import RegistrationPage from "./pages/registration/RegistrationPage";
+import DashboardPage from "./pages/dashboard/DashboardPage";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegistrationPage />} />
+                <Route path="/dashboard" element={<DashboardPage />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/pages/registration/RegistrationPage.tsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+import {Link} from "react-router-dom";
+import axios from "axios";
+export default function RegistrationPage() {
+
+    const [values, setValues] = useState({ email: "", password: "" });
+    useEffect(() => {
+
+    });
+
+
+    const handleSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+
+        try {
+            const { data } = await axios.post(
+                "http://localhost:4000/register",
+                {
+                    ...values,
+                },
+                { withCredentials: true }
+            );
+            console.log(data)
+
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    return (
+        <div className="container">
+            <h2>Register Account</h2>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        onChange={(e) =>
+                            setValues({ ...values, [e.target.name]: e.target.value })
+                        }
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        onChange={(e) =>
+                            setValues({ ...values, [e.target.name]: e.target.value })
+                        }
+                    />
+                </div>
+                <button type="submit">Submit</button>
+                <span>
+          Already have an account ?<Link to="/login"> Login</Link>
+        </span>
+            </form>
+            <ToastContainer />
+        </div>
+    );
+}
